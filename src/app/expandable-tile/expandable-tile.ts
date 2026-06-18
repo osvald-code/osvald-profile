d8af5c9e16302535fe7e74eb7b18b48905f272b0
ulty <47011082+osvald-code@users.noreply.github.com>
2026-06-17 23:00:11 -0400

    overlay component rough added

--- src/app/expandable-tile/expandable-tile.ts
+++ src/app/expandable-tile/expandable-tile.ts
@@ -1,10 +1,10 @@
-import { Component, computed, input, Input, inject} from '@angular/core';
+import { Component, computed, input, Input, inject, signal} from '@angular/core';
 import {CdkDrag, CdkDragPlaceholder} from '@angular/cdk/drag-drop';
 import { TileManager } from '../tile-manager';
-import { Tile } from '../tile';
+import { OverlayModule } from '@angular/cdk/overlay';
 @Component({
   selector: 'expandable-tile',
-  imports: [CdkDrag, CdkDragPlaceholder],
+  imports: [CdkDrag, CdkDragPlaceholder,OverlayModule],
   templateUrl: './expandable-tile.html',
   styleUrl: './expandable-tile.scss',
 })
@@ -14,10 +14,17 @@ export class ExpandableTile{
   public title = input<string>('');
   @Input() id:number = 0;
   tileManager = inject(TileManager);
-
+  isOverlayVisible = signal(false);
   isVisible = computed(() => this.title() === '');
   innerWidth = this.tileManager.tileInnerSize; 
   innerHeight = this.innerWidth; //its a square lol
   tileWidth = this.tileManager.tileOuterSize;
   tileHeight = this.tileWidth
-}
+
+  showOverlay = ():void =>{
+    this.isOverlayVisible.set(!this.isOverlayVisible())
+      console.log("Overlay: ",this.isOverlayVisible());
+    }
+    
+  };
+
