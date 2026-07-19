66eaf01a7a9ac5cb2d6669b3c55524190693d12a
ulty <47011082+osvald-code@users.noreply.github.com>
2026-07-18 22:46:48 -0400

    firebase deployment config added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
 };
+
